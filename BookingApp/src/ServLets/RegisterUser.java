89be0e5b82cf5b27496a5b2dc3bf1c98d8584a97
Stefania <stefanoula-fani@hotmail.com>
2017-09-16 02:03:47 +0300

    Servlets Fixed

---
+++ BookingApp/src/ServLets/RegisterUser.java
@@ -0,0 +1,54 @@
+package ServLets;
+
+
+import JavaFiles.JavaMysqlSelect;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/RegisterUser")
+public class RegisterUser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegisterUser() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("INSIDE RegisterUser");
+    	
+    	
+    	//System.out.println(request.getParameter("submit"));
+        if (request.getParameter("submit") != null) {
+        	
+        } else {
+        	System.out.println("GUFTOS");
+        }
+
+        //request.getRequestDispatcher("/WEB-INF/some-result.jsp").forward(request, response);
+	}
+
+}
