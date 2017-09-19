7001d5405afc53816f7a5295bfb06dd3e2574c94
Stefania <stefanoula-fani@hotmail.com>
2017-09-20 02:42:26 +0300

    register almoste done

--- BookingApp/src/JavaFiles/HandleUser.java
+++ BookingApp/src/JavaFiles/HandleUser.java
@@ -1,13 +1,25 @@
 package JavaFiles;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+
+import Entities.Users;
 
 public class HandleUser {
 
-	public static void insertUser()
+	public static int insertUser(Users newUser)
 	{
 		System.out.println("INSIDE insertUser");
 
@@ -20,29 +32,89 @@ public class HandleUser {
 			String myUrl = "jdbc:mysql://localhost:3306/mydb";
 			Class.forName(myDriver);
 			Connection conn = DriverManager.getConnection(myUrl, "root", "root1234");
-
-			System.out.println("INSIDE TRYYYY " + conn.isValid(1000));
-
-
-			String query ="INSERT INTO User (user_id, username, password, name, surname, email, tel, role_host, role_tenant, role_admin,address_number,street_address,city, country, postal_code,birthday,comment_id) "+
-			"VALUES (10, 'evagg', '1@3$kk2g', 'Evaggelos', 'Papadopoulos', 'evagPap@gmail.com', 6923673211, 0, 0, 1,  12, 'Efstratiou', 'Thessaloniki', 'Ellada', 11098,  06/07/1975,112)";
-
-			Statement stmt = conn.createStatement();
-			int colCount = stmt.executeUpdate(query);
 			
-
-			if(colCount==-1) {
-				System.out.println("User already exists.");
+			int hostBoolean;
+			int tenantBoolean;
+			if(newUser.getHost()==null) {
+				hostBoolean = 0;
 			}
 			else {
-				System.out.println("Insert completed successfully!");
+				hostBoolean = 1;
 			}
-			stmt.close();
+			if(newUser.getTenant()==null) {
+				tenantBoolean = 0;
+			}
+			else {
+				tenantBoolean = 1;
+			}
+
+			Statement stmt = conn.createStatement();
+
+			//check for same username before insert
+			String checkQuery = "SELECT username FROM User WHERE username=" + "'" + newUser.getUsername() + "'";
+			ResultSet rs = stmt.executeQuery(checkQuery);
+			rs.beforeFirst();
+			int rowCount = rs.last() ? rs.getRow() : 0;
+
+			
+			if(rowCount==0) {
+				//insert query
+				System.out.println("BYTE ARRAY: " + convertImageToBArray(newUser.getImagePath()) );
+				
+				String insertQuery = "INSERT INTO User (username, password, name, surname, email, tel, role_host, role_tenant, photo, address_number, street_address, city, country, postal_code, birthday, comment_id) "
+						+ "VALUES ("
+						+ "'" + newUser.getUsername() + "'" +  ","
+						+ "'" + newUser.getPassword() + "'" +  ","
+						+ "'" + newUser.getName() + "'" +  ","
+						+ "'" + newUser.getSurname() + "'" +  ","
+						+ "'" + newUser.getEmail() + "'" +  ","
+						+ newUser.getTel() +  ","
+						+ hostBoolean +  ","
+						+ tenantBoolean +  ","
+						+ "'" + convertImageToBArray(newUser.getImagePath()) + "'" +  ","
+						+ newUser.getAddressNumber() +  ","
+						+ "'" + newUser.getStreetAddress() + "'" +  ","
+						+ "'" + newUser.getCity() + "'" +  ","
+						+ "'" + newUser.getCountry() + "'" +  ","
+						+ newUser.getPostalCode() +  ","
+						+ "STR_TO_DATE( '" + newUser.getBirthday() +  "'," + " '%d/%m/%Y'),"
+						+ 112 + ")";
+
+				int colCount = stmt.executeUpdate(insertQuery);
+
+
+				if(colCount==-1) {
+					System.out.println("An error has occured.");
+				}
+				else {
+					System.out.println("Insert completed successfully!");
+				}
+				stmt.close();
+				return 0;
+			}
+			else {
+				System.out.println("User already exists.");
+				return 1;
+			}
 		}
 		catch (Exception e)
 		{
 			System.err.println("Got an SQL exception! ");
 			System.err.println(e.getMessage());
+			return 2;
 		}
+	}
+
+	private static byte[] convertImageToBArray(String ImageName) throws IOException {
+
+		// open image
+		File imgPath = new File(ImageName);
+		BufferedImage bufferedImage = ImageIO.read(imgPath);
+
+		// get DataBufferBytes from Raster
+		WritableRaster raster = bufferedImage .getRaster();
+		DataBufferByte data   = (DataBufferByte) raster.getDataBuffer();
+
+		return ( data.getData() );
 	}
 }
