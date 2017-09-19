c6c23d3764a671358465faca59ee248e48ed14df
Stefania <stefanoula-fani@hotmail.com>
2017-09-20 02:41:57 +0300

    register almost done

--- BookingApp/src/ServLets/RegisterUser.java
+++ BookingApp/src/ServLets/RegisterUser.java
@@ -5,11 +5,15 @@ import JavaFiles.HandleUser;
 import JavaFiles.JavaMysqlSelect;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import Entities.Users;
 
 /**
  * Servlet implementation class MyServlet
@@ -17,14 +21,14 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/RegisterUser")
 public class RegisterUser extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RegisterUser() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public RegisterUser() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -39,19 +43,61 @@ public class RegisterUser extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("INSIDE RegisterUser");
-    	HandleUser handleUser = new HandleUser();
 		
-    	//System.out.println(request.getParameter("submit"));
-        if (request.getParameter("submit") != null) {
-        	handleUser.insertUser();
-        } else {
-        	System.out.println("Submit was unsuccessfull!!");
-        }
-        
-        response.sendRedirect("./html files/register_done.html");
+		HandleUser handleUser = new HandleUser();
 
-        //request.getRequestDispatcher("/WEB-INF/some-result.jsp").forward(request, response);
+		//get fields
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String email = request.getParameter("email");
+		String name = request.getParameter("name");
+		String surname = request.getParameter("surname");
+		String tel = request.getParameter("tel");
+		int telNumber = 0;
+		if(tel.length()!=0) {
+			telNumber = Integer.parseInt(tel);
+		}
+		String tenant = request.getParameter("tenant");
+		String host = request.getParameter("host");
+		String birthday = request.getParameter("birthday");
+		String imagePath = request.getParameter("imagePath");
+		String country = request.getParameter("country");
+		String city = request.getParameter("city");
+
+		String postalCode = request.getParameter("postal_code");
+		int postalCodeNumber = 0;
+		if(postalCode.length()!=0) {
+			postalCodeNumber = Integer.parseInt(postalCode);
+		}
+
+		String streetAddress = request.getParameter("street_address");
+
+		String addressNumber = request.getParameter("address_number");
+		int addressNumberContent = 0;
+		if(addressNumber.length()!=0) {
+			addressNumberContent = Integer.parseInt(addressNumber);
+		}
+
+		Users newUser = new Users(username,password,email,name,surname,telNumber,birthday,imagePath,country,city,postalCodeNumber,streetAddress,addressNumberContent,host,tenant);
+
+		//System.out.println(request.getParameter("submit"));
+		if (request.getParameter("submit") != null) {
+			if((handleUser.insertUser(newUser))==0) {
+				System.out.println("Submit was successfull!!");
+				response.sendRedirect("./html files/register_done.html");
+			}else {
+				PrintWriter out = response.getWriter();
+				response.setContentType("text/html;charset=UTF-8");
+				out.println("<script type=text/javascript>");
+				out.println("alert('Username already exists. бссс');");
+				out.println("location.href='html files/register.html';");
+				out.println("</script>");
+			}
+		}
+		else {
+			System.out.println("Submit was unsuccessfull!!");
+		}
+
 	}
 
 }
