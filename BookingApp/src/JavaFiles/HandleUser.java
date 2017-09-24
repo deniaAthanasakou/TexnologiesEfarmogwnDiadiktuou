9c8069a470449d79014a5489f71e2e9c8bab80b5
Stefania <stefanoula-fani@hotmail.com>
2017-09-24 13:46:15 +0300

    register officially done

--- BookingApp/src/JavaFiles/HandleUser.java
+++ BookingApp/src/JavaFiles/HandleUser.java
@@ -1,48 +1,24 @@
 package JavaFiles;
 
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
-import java.awt.image.WritableRaster;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
+import java.text.SimpleDateFormat;
 
-
-import Entities.Users;
+import Entities.User;
 
 public class HandleUser {
 
-	public static int insertUser(Users newUser)
+	public static int insertUser(User newUser)
 	{
 		try
 		{
 			// create our mysql database connection
 			String myDriver = "com.mysql.jdbc.Driver";
-			String myUrl = "jdbc:mysql://localhost:3306/mydb";
+			String myUrl = "jdbc:mysql://localhost:3306/mydb?useSSL=true";
 			Class.forName(myDriver);
 			Connection conn = DriverManager.getConnection(myUrl, "root", "root1234");
-			
-			int hostBoolean;
-			int tenantBoolean;
-			if(newUser.getHost()==null) {
-				hostBoolean = 0;
-			}
-			else {
-				hostBoolean = 1;
-			}
-			if(newUser.getTenant()==null) {
-				tenantBoolean = 0;
-			}
-			else {
-				tenantBoolean = 1;
-			}
 
 			Statement stmt = conn.createStatement();
 
@@ -51,49 +27,53 @@ public class HandleUser {
 			ResultSet rs = stmt.executeQuery(checkQuery);
 			rs.beforeFirst();
 			int rowCount = rs.last() ? rs.getRow() : 0;
-
+			
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			String afterFormat = null;
+			if(newUser.getBirthday()!=null) {
+				afterFormat = sdf.format(newUser.getBirthday());
+			}
+		
 			if(rowCount==0) {
+				
 				//insert query
-				String insertQuery;
-				if(newUser.getImagePath() != null) {
-					insertQuery = "INSERT INTO User (username, password, name, surname, email, tel, role_host, role_tenant, photo, address_number, street_address, city, country, postal_code, birthday, comment_id) "
-							+ "VALUES ("
-							+ "'" + newUser.getUsername() + "'" +  ","
-							+ "'" + newUser.getPassword() + "'" +  ","
-							+ "'" + newUser.getName() + "'" +  ","
-							+ "'" + newUser.getSurname() + "'" +  ","
-							+ "'" + newUser.getEmail() + "'" +  ","
-							+ newUser.getTel() +  ","
-							+ hostBoolean +  ","
-							+ tenantBoolean +  ","
-							+ "'" + convertImageToBArray(newUser.getImagePath()) + "'" +  ","
-							+ newUser.getAddressNumber() +  ","
-							+ "'" + newUser.getStreetAddress() + "'" +  ","
-							+ "'" + newUser.getCity() + "'" +  ","
-							+ "'" + newUser.getCountry() + "'" +  ","
-							+ newUser.getPostalCode() +  ","
-							+ "STR_TO_DATE( '" + newUser.getBirthday() +  "'," + " '%d/%m/%Y'),"
-							+ 112 + ")";
-
+				String insertQuery = "INSERT INTO User (username, password, name, surname, email, tel, role_host, role_tenant,";
+				if(newUser.getPhoto()!=null) {
+					insertQuery +=  "photo,";  
+				}
+				insertQuery += "address_number, street_address, city, country, postal_code";
+				if(newUser.getBirthday()!=null) {
+					insertQuery += ",birthday)";
 				}
 				else {
-					insertQuery = "INSERT INTO User (username, password, name, surname, email, tel, role_host, role_tenant, address_number, street_address, city, country, postal_code, birthday, comment_id) "
-							+ "VALUES ("
-							+ "'" + newUser.getUsername() + "'" +  ","
-							+ "'" + newUser.getPassword() + "'" +  ","
-							+ "'" + newUser.getName() + "'" +  ","
-							+ "'" + newUser.getSurname() + "'" +  ","
-							+ "'" + newUser.getEmail() + "'" +  ","
-							+ newUser.getTel() +  ","
-							+ hostBoolean +  ","
-							+ tenantBoolean +  ","
-							+ newUser.getAddressNumber() +  ","
-							+ "'" + newUser.getStreetAddress() + "'" +  ","
-							+ "'" + newUser.getCity() + "'" +  ","
-							+ "'" + newUser.getCountry() + "'" +  ","
-							+ newUser.getPostalCode() +  ","
-							+ "STR_TO_DATE( '" + newUser.getBirthday() +  "'," + " '%d/%m/%Y'),"
-							+ 112 + ")";
+					insertQuery += ")";
+				}
+				
+				insertQuery += " VALUES ("
+						+ "'" + newUser.getUsername() + "'" +  ","
+						+ "'" + newUser.getPassword() + "'" +  ","
+						+ "'" + newUser.getName() + "'" +  ","
+						+ "'" + newUser.getSurname() + "'" +  ","
+						+ "'" + newUser.getEmail() + "'" +  ","
+						+ "'" + newUser.getTel() + "'" +  ","
+						+ newUser.getRoleHost() +  ","
+						+ newUser.getRoleTenant() +  ",";
+				
+				if(newUser.getPhoto()!=null) {
+					insertQuery += "'" + newUser.getPhoto() + "'" +  "," ; 
+				}
+				
+				insertQuery += newUser.getAddressNumber() +  ","
+						+ "'" + newUser.getStreetAddress() + "'" +  ","
+						+ "'" + newUser.getCity() + "'" +  ","
+						+ "'" + newUser.getCountry() + "'" +  ","
+						+ "'" + newUser.getPostalCode() + "'";
+				
+				if(newUser.getBirthday()!=null) {
+					insertQuery += "," + "'" + afterFormat + "'" + ")";
+				}
+				else {
+					insertQuery += ")";
 				}
 				
 				int colCount = stmt.executeUpdate(insertQuery);
@@ -117,18 +97,5 @@ public class HandleUser {
 			System.err.println(e.getMessage());
 			return 2;
 		}
-	}
-
-	private static byte[] convertImageToBArray(String ImageName) throws IOException {
-
-		// open image
-		File imgPath = new File(ImageName);
-		BufferedImage bufferedImage = ImageIO.read(imgPath);
-
-		// get DataBufferBytes from Raster
-		WritableRaster raster = bufferedImage .getRaster();
-		DataBufferByte data   = (DataBufferByte) raster.getDataBuffer();
-
-		return ( data.getData() );
 	}
 }
