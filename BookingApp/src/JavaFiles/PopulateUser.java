5a6bcd51a29a851b1b336072a22c5fae5df06f2e
Stefania <stefanoula-fani@hotmail.com>
2017-09-27 15:56:41 +0300

    Add files via upload

--- BookingApp/src/JavaFiles/PopulateUser.java
+++ BookingApp/src/JavaFiles/PopulateUser.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class ImportUsersAdmin {
+public class PopulateUser {
 
-	public ResultSet populateAdminHandle()
+	public static int populateProfile(String username)
 	{
 		try
 		{
@@ -20,18 +20,29 @@ public class ImportUsersAdmin {
 			Statement stmt = conn.createStatement();
 
 			//check for same username before insert
-			String results = "SELECT username,name,surname,role_host,role_tenant,approved FROM User";
-			ResultSet rs = stmt.executeQuery(results);
+			String checkQuery = "SELECT * FROM User WHERE username=" + "'" + username + "'";
+			ResultSet rs = stmt.executeQuery(checkQuery);
+			stmt.close();
 			rs.beforeFirst();
-			
-			return rs;
+			int rowCount = rs.last() ? rs.getRow() : 0;
+		
+			if(rowCount==0) {
+				
+				//populate profile 
+				
+				
+				return 0;
+			}
+			else {
+				System.out.println("User does not exist.");
+				return 1;
+			}
 		}
 		catch (Exception e)
 		{
 			System.err.println("Got an SQL exception! ");
 			System.err.println(e.getMessage());
-			return null;
+			return 2;
 		}
-		
 	}
 }
